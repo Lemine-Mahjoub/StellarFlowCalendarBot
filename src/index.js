104a7440c3017c044faa8b061f45c32904069dff
Lemine-Mahjoub <leminemahjoub@gmail.com>
2025-02-25 08:17:24 +0000

    :sparkles: ID Server :sparkles:

--- src/index.js
+++ src/index.js
@@ -14,65 +14,67 @@ client.once("ready", () => {
     console.log(`✅ Connecté en tant que ${client.user.tag}`);
 });
 
-client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
-    const args = message.content.split(" ");
-    const command = args.shift().toLowerCase();
+if (message.guildId === "1328878024904671232" || message.guildId === "1343854698146562088") {
+    client.on("messageCreate", async (message) => {
+        if (message.author.bot) return;
+        const args = message.content.split(" ");
+        const command = args.shift().toLowerCase();
 
-    if (command === commandes.profil) {
-        profil(message);
-    }
-    if (command === commandes.semaine) {
-        semaine(message);
-    }
+        if (command === commandes.profil) {
+            profil(message);
+        }
+        if (command === commandes.semaine) {
+            semaine(message);
+        }
 
-    if (command === commandes.challenge) {
-        challenge(message);
-    }
+        if (command === commandes.challenge) {
+            challenge(message);
+        }
 
-    if (command === commandes.addChallenge && isAdmin(message)) {
-        addChallenge(message, args);
-    }
+        if (command === commandes.addChallenge && isAdmin(message)) {
+            addChallenge(message, args);
+        }
 
-    if (command === commandes.deleteChallenge && isAdmin(message)) {
-        deleteChallenge(message, args);
-    }
+        if (command === commandes.deleteChallenge && isAdmin(message)) {
+            deleteChallenge(message, args);
+        }
 
-    if (command === commandes.addAnim && isAdmin(message)) {
-        addAnimation(message, args);
-    }
+        if (command === commandes.addAnim && isAdmin(message)) {
+            addAnimation(message, args);
+        }
 
-    if (command === commandes.deleteAnim && isAdmin(message)) {
-        deleteAnimation(message, args);
-    }
+        if (command === commandes.deleteAnim && isAdmin(message)) {
+            deleteAnimation(message, args);
+        }
 
-    if (command === commandes.badge) {
-        badge(message);
-    }
+        if (command === commandes.badge) {
+            badge(message);
+        }
 
-    if (command === commandes.addBadge && isAdmin(message)) {
-        addBadge(message, args);
-    }
+        if (command === commandes.addBadge && isAdmin(message)) {
+            addBadge(message, args);
+        }
 
-    if (command === commandes.deleteBadge && isAdmin(message)) {
-        deleteBadge(message, args);
-    }
+        if (command === commandes.deleteBadge && isAdmin(message)) {
+            deleteBadge(message, args);
+        }
 
-    if (command === commandes.anim) {
-        anim(message);
-    }
+        if (command === commandes.anim) {
+            anim(message);
+        }
 
-    if (command === commandes.help) {
-        help(message);
-    }
+        if (command === commandes.help) {
+            help(message);
+        }
 
-    if (command === commandes.assignBadge && isAdmin(message)) {
-        assignBadge(message, args);
-    }
+        if (command === commandes.assignBadge && isAdmin(message)) {
+            assignBadge(message, args);
+        }
 
-    if (command === commandes.unassignBadge && isAdmin(message)) {
-        unassignBadge(message, args);
-    }
-});
+        if (command === commandes.unassignBadge && isAdmin(message)) {
+            unassignBadge(message, args);
+        }
+    });
+}
 
 client.login(process.env.DISCORD_TOKEN);
